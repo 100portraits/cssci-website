3fb3e5412c08e87fb27c90f823a37cc2b2829251
sahir <sahirde@gmail.com>
2025-09-26 21:58:58 +0200

    fix: update iconMap type to use React.ReactElement and import React

--- src/app/impact/page.tsx
+++ src/app/impact/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import React from 'react'
 import { client } from '@/sanity/lib/client'
 import { IMPACT_PAGE_QUERY } from '@/sanity/lib/queries'
 import PortableTextContent from '@/components/PortableTextContent'
@@ -17,7 +18,7 @@ async function getImpactData() {
 }
 
 // Icon mapping for impact cards
-const iconMap: { [key: string]: JSX.Element } = {
+const iconMap: { [key: string]: React.ReactElement } = {
   chart: (
     <svg className="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
